527a46ac496d12564e0828b4690a67b23fcffc25
trungnvhe160288 <trungnvhe160288@fpt.edu.vn>
2023-05-11 17:22:54 +0700

    trungnv: create student.java in "model" package

---
+++ SWP391_SU23_Group1_OnlineShop/src/main/java/com/fptuniversity/swp391_su23_group1_onlineshop/model/student.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fptuniversity.swp391_su23_group1_onlineshop.model;
+
+/**
+ *
+ * @author trung
+ */
+public class student {
+    private int age;
+    private String name;
+}
