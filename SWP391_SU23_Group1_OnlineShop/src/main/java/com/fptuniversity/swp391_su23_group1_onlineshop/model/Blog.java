eea4c283166b51f5b04dd865966403e695b586d2
lehongphuc33 <phuclhhe153529@fpt.edu.vn>
2023-05-24 18:30:22 +0700

    PhucLH: Add Blog model

---
+++ SWP391_SU23_Group1_OnlineShop/src/main/java/com/fptuniversity/swp391_su23_group1_onlineshop/model/Blog.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fptuniversity.swp391_su23_group1_onlineshop.model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Blog {
+    
+}
