318167737b195422e5c62ab8f567daa3cc7860fe
lehongphuc33 <phuclhhe153529@fpt.edu.vn>
2023-05-24 22:03:24 +0700

    PhucLH : Add comment for Blog model.

--- SWP391_SU23_Group1_OnlineShop/src/main/java/com/fptuniversity/swp391_su23_group1_onlineshop/model/Blog.java
+++ SWP391_SU23_Group1_OnlineShop/src/main/java/com/fptuniversity/swp391_su23_group1_onlineshop/model/Blog.java
@@ -4,10 +4,47 @@
  */
 package com.fptuniversity.swp391_su23_group1_onlineshop.model;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Admin
  */
 public class Blog {
-    
+    /**
+     * The unique identifier for this blog post.
+     */
+    private int id;
+
+    /**
+     * The title of this blog post.
+     */
+    private String title;
+
+    /**
+     * The author of this blog post.
+     */
+    private String author;
+
+    /**
+     * The date and time that this blog post was published.
+     */
+    private LocalDateTime publishDateTime;
+
+    /**
+     * The content of this blog post.
+     */
+    private String content;
+
+    /**
+     * The category that this blog post belongs to.
+     */
+    private String category;
+
+    /**
+     * The URL of the image associated with this blog post.
+     */
+    private String imageUrl;
+
+    // Constructor, getters, and setters omitted for brevity
 }
