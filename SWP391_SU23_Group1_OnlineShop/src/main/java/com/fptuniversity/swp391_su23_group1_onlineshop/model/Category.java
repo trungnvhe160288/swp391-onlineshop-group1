7b5323eac058535c33fa7fbbcfadd96cab337cc1
Linhnvbhe153459 <linhnvbhe153459@fpt.edu.vn>
2023-05-29 13:10:55 +0700

    LinhNVB: update Category.java attributes

--- SWP391_SU23_Group1_OnlineShop/src/main/java/com/fptuniversity/swp391_su23_group1_onlineshop/model/Category.java
+++ SWP391_SU23_Group1_OnlineShop/src/main/java/com/fptuniversity/swp391_su23_group1_onlineshop/model/Category.java
@@ -1,74 +1,36 @@
+package com.fptuniversity.swp391_su23_group1_onlineshop.model;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.fptuniversity.swp391_su23_group1_onlineshop.model;
-
-/**
- *
- * @author Baoli
- */
 public class Category {
-    private String shirt;
-    private String pant;
-    private String shoe;
-    private String hat;
-    private String bag;
+
+    private String id;
+    private String name;
 
     public Category() {
     }
 
-    public Category(String shirt, String pant, String shoe, String hat, String bag) {
-        this.shirt = shirt;
-        this.pant = pant;
-        this.shoe = shoe;
-        this.hat = hat;
-        this.bag = bag;
-    }    
-    
-    public String getShirt() {
-        return shirt;
+    public Category(String id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    public void setShirt(String shirt) {
-        this.shirt = shirt;
+    public String getId() {
+        return id;
     }
 
-    public String getPant() {
-        return pant;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public void setPant(String pant) {
-        this.pant = pant;
+    public String getName() {
+        return name;
     }
 
-    public String getShoe() {
-        return shoe;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setShoe(String shoe) {
-        this.shoe = shoe;
-    }
-
-    public String getHat() {
-        return hat;
-    }
-
-    public void setHat(String hat) {
-        this.hat = hat;
-    }
-
-    public String getBag() {
-        return bag;
-    }
-
-    public void setBag(String bag) {
-        this.bag = bag;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" + "shirt=" + shirt + ", pant=" + pant + ", shoe=" + shoe + ", hat=" + hat + ", bag=" + bag + '}';
-    }    
-    
 }
