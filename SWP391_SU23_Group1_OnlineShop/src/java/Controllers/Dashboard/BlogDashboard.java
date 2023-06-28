52298bbf3702f4b4ab3a03c234e6920b3b7c5339
lehongphuc33 <phuclhhe153529@fpt.edu.vn>
2023-06-29 00:20:07 +0700

    PhucLH : Process get list in pagination

--- SWP391_SU23_Group1_OnlineShop/src/java/Controllers/Dashboard/BlogDashboard.java
+++ SWP391_SU23_Group1_OnlineShop/src/java/Controllers/Dashboard/BlogDashboard.java
@@ -23,5 +23,42 @@ import java.util.List;
 @WebServlet(name = "BlogDashboard", urlPatterns = {"/dashboard/blog"})
 public class BlogDashboard extends HttpServlet {
 
-    
+    //process pagination for blog dashboard
+    private void pagination(HttpServletRequest request, HttpServletResponse response) {
+        //Call BlogDAO 
+        BlogDAO bd = new BlogDAO();
+        List<Blog> data = bd.getAll();
+        
+        //Process redirect pagination
+        String xpage = request.getParameter("page");
+
+        int page = (xpage == null) ? 1 : Common.parseInt(xpage);
+        int numberPerPage = 8;
+        int size = data.size();
+        int numberOfPage = ((size % numberPerPage == 0) ? (size / numberPerPage) : (size / numberPerPage + 1));
+        int start = (page - 1) * numberPerPage;
+        int end = Math.min(page * numberPerPage, size);
+        //Call BlogDAO to get list need to show
+        List<Blog> listByPage = bd.getListByPage(data, start, end);
+        
+        //Set data for view 
+        request.setAttribute("page", page);
+        request.setAttribute("numberOfPage", numberOfPage);
+        request.setAttribute("start", start + 1);
+        request.setAttribute("end", end);
+        request.setAttribute("numberPerPage", numberPerPage);
+
+        request.setAttribute("data", listByPage);
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
